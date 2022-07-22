e59641519485e8eac3ea871cc57fe1c166b19af2
Courtney Azar <courtney.a.azar@gmail.com>
2022-07-22 17:15:53 -0500

    css updated for about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -7,6 +7,7 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
+        <img src="/images/Vetelligence-02.svg"/>
         <h4>About Us</h4>
         <p>Our Founder, Zachary, served in the United States Army infantry, which included a deployment to Afghanistan. Upon exiting the military, he experienced a challenging and frustrating job application process to enter the workforce. After filling out over 350 applications and facing the same inefficient and ineffective process over and over for months at a time, the idea of what would become Vetelligence was born.</p>
         <p>Our AI/ML platform is engineered to remove barriers, create opportunities and forge relationships. Unfortunately, many AI companies have benefited from the perception that they’ve built sophisticated automation and A.I., rather than a system that relies on manual labor. The landscape for sourcing top talent has evolved to which we are equipped to assist employers with the data backed hiring. You don’t need to be a recruiting expert to reimagine the hiring process - just someone who cares about who is coming in the door.</p>
