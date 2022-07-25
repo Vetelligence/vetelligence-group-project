da993dee4e621df0180808be8ba61627c2d693cc
Jonathan Palalay <ring5pun@hotmail.com>
2022-07-25 11:26:29 -0500

    added company input box

--- src/components/EmployerIntakeForm/EmployerIntakeForm.jsx
+++ src/components/EmployerIntakeForm/EmployerIntakeForm.jsx
@@ -6,8 +6,8 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import { useHistory, useDispatch } from 'react-router-dom';
-
+import { useHistory} from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 function EmployerIntakeForm(){
 
@@ -18,31 +18,67 @@ function EmployerIntakeForm(){
         history.push('/')
     }
 
-    
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [company, setCompany] = useState('');
+    const [email, setEmail] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (evt) => {
+        const employerInfo = {
+            firstName: firstName,
+            lastName: lastName,
+            company: company,
+            email: email,
+            phoneNumber: phoneNumber,
+            city: city,
+            state: state,
+            username: username,
+            password, password,
+        }
+
+        evt.preventDefault();
+
+        dispatch({
+            type: "SET_EMPLOYER_INFO",
+            payload: employerInfo
+        })
+
+        
+        
+
+    }
 
     return (
         <>
         <Button variant="outlined" onClick={backButton}>Back</Button>
 
-        <form>
-        <TextField id="outlined-basic" label="First Name" variant="outlined" />
+        <form onSubmit={handleSubmit}>
+
+        <TextField id="outlined-basic" label="First Name" variant="outlined" onChange={evt => {setFirstName(evt.target.value)}}/>
         <br></br>
-        <TextField id="outlined-basic" label="Last Name" variant="outlined" />
+        <TextField id="outlined-basic" label="Last Name" variant="outlined" onChange={evt => {setLastName(evt.target.value)}}/>
         <br></br>
-        <TextField id="outlined-basic" label="E-mail" variant="outlined" />
+        <TextField id="outlined-basic" label="Company" variant="outlined" onChange={evt => {setCompany(evt.target.value)}}/>
         <br></br>
-        <TextField id="outlined-basic" label="Phone Number" variant="outlined" />
+        <TextField id="outlined-basic" label="E-mail" variant="outlined" onChange={evt => {setEmail(evt.target.value)}}/>
         <br></br>
-        <TextField id="outlined-basic" label="City" variant="outlined" />
+        <TextField id="outlined-basic" label="Phone Number" variant="outlined" onChange={evt => {setPhoneNumber(evt.target.value)}}/>
         <br></br>
-        <TextField id="outlined-basic" label="State" variant="outlined" />
+        <TextField id="outlined-basic" label="City" variant="outlined" onChange={evt => {setCity(evt.target.value)}}/>
         <br></br>
-        <TextField id="outlined-basic" label="Username" variant="outlined" />
+        <TextField id="outlined-basic" label="State" variant="outlined" onChange={evt => {setState(evt.target.value)}}/>
         <br></br>
-        <TextField id="outlined-basic" label="Password" variant="outlined" />
+        <TextField id="outlined-basic" label="Username" variant="outlined" onChange={evt => {setUsername(evt.target.value)}}/>
+        <br></br>
+        <TextField id="outlined-basic" label="Password" variant="outlined" onChange={evt => {setPassword(evt.target.value)}}/>
         <br></br>
 
-        <Button variant="contained" color="primary" type="Submit">
+        <Button variant="contained" color="primary" type="submit">
             Submit
         </Button>
 
