0f84c15e4dd6ec7d4fe062fcd233be065e1e57fb
Courtney Azar <courtney.a.azar@gmail.com>
2022-07-27 10:13:16 -0500

    jobs router created.

---
+++ src/redux/sagas/job.saga.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+function* fetchJob(action){
+    console.log('in fetchJob', action);
+    try {
+        const res = yield axios.get(`/api/job/${action.payload}`);
+        yield put({ type: 'SET_JOB', payload: res.data });
+    } catch (err) {
+        console.log('fetchJob request failed', err);
+        return;
+    }
+}
+
+function* addJob(action){
+    try{ 
+        yield axios.post('/api/job', action.payload)
+    }
+    catch(err){
+        console.error('error is', err)
+    }
+}
+
+function* fetchSkills(action){
+    console.log('in fetchSkills', action);
+    try {
+        const res = yield axios.get(`/api/job/${action.payload}`);
+        yield put({ type: 'SET_JOB', payload: res.data });
+    } catch (err) {
+        console.log('fetchSkills request failed', err);
+        return;
+    }
+}
+
+function* employerSaga() {
+    yield takeLatest('FETCH_JOB', fetchJob);
+    yield takeLatest('ADD_JOB', addJob);
+    yield takeLatest('FETCH_SKILLS', fetchSkills);
+  }
+
+export default employerSaga;
