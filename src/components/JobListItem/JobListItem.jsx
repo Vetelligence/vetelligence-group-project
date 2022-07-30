4ad08b289493c6ad66117f2aa883c24dfca06c4e
Joey Lundquist <joeylundquist91@gmail.com>
2022-07-30 15:39:50 -0500

    added a classname on the landing page h2 to stop the h2's on every other page from gaining its properties in the landpage.css

--- src/components/JobListItem/JobListItem.jsx
+++ src/components/JobListItem/JobListItem.jsx
@@ -2,6 +2,12 @@ import EditIcon from '@material-ui/icons/Edit';
 import CancelPresentationIcon from '@material-ui/icons/CancelPresentation';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+
 
 export const JobListItem = ({jobs}) => {
     const dispatch = useDispatch();
@@ -12,13 +18,34 @@ export const JobListItem = ({jobs}) => {
             payload: {id: jobs.id}
         })
     }
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => {
+      setOpen(true);
+    };
+    const handleClose = () => {
+      setOpen(false);
+    };
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        pt: 2,
+        px: 4,
+        pb: 3,
+      };
 
     return(
         <div className="jobCard">
             <EditIcon />
-            <CancelPresentationIcon/>
-            <p className="jobCardText" onClick={grabMatchedCandidates}>Title: {jobs && jobs.job_name}</p>
-            {/* <p className="jobCardText">{jobs && jobs.job_description}</p> */}
+            <Button onClick={handleOpen}><CancelPresentationIcon /></Button>
+          
+
+            <p className="jobCardText" onClick={grabMatchedCandidates}>Job Title: {jobs && jobs.job_name}</p>
         </div>
     )
 }
