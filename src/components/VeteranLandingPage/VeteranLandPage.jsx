a72784fbb85995b200851ed3113c6f71d4a510dc
Courtney Azar <courtney.a.azar@gmail.com>
2022-08-03 10:16:44 -0500

    Fixed the carousel

--- src/components/VeteranLandingPage/VeteranLandPage.jsx
+++ src/components/VeteranLandingPage/VeteranLandPage.jsx
@@ -5,19 +5,20 @@ import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './VeteranLanding.css'
 
 function VeteranLandingPage() {
     const dispatch = useDispatch();
     const history = useHistory();
     const jobs = useSelector((store) => store.job);
-
+    const [index, setIndex] = useState(0)
+    console.log(jobs, ">>>>>>>>>>")
     useEffect(() => {
         dispatch({
           type: 'FETCH_TOP_JOBS',
         })
-      }, []); 
+      }, [index]); 
 
     function Login() {
         history.push('/veteran-intake');
@@ -28,14 +29,28 @@ function VeteranLandingPage() {
             <br></br>
             <h4>Jobs Available to Veterans</h4>
             <div className="carousel">
-                <Button><ArrowBackIosIcon/></Button>
-                {jobs && jobs.map((job) => (
-                    <div className="carouselItem" key={job.id}>
-                        <p>Job Title: {job.job_name}</p>
-                        <p>Job Description: {job.job_description}</p>
-                    </div>
-                ))}
-                <Button><ArrowForwardIosIcon/></Button>
+                <Button
+                    onClick={() => 
+                        index>0 ?
+                        setIndex(index - 1)
+                        :
+                        setIndex(4)
+                    }>
+                    <ArrowBackIosIcon/>
+                </Button>
+                <div className="carouselItem" key={jobs[0] && jobs[index].id}>
+                    <p>Job Title: {jobs[0] && jobs[index].job_name}</p>
+                    <p>Job Description: {jobs[0] && jobs[index].job_description}</p>
+                </div>
+                <Button
+                    onClick={() =>
+                        index<4 ?
+                        setIndex(index + 1)
+                        :
+                        setIndex(0)
+                    }>
+                    <ArrowForwardIosIcon/>
+                </Button>
             </div>
             <br></br>
             <Button className="btn" variant="outlined" color="primary" >
